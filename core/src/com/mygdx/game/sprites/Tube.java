74f313fed6c9815ba249d614ffbce2b07b52f5bf
NinpoU-u <myse1lff@gmail.com>
2019-03-22 20:52:38 +0200

    Added Tube class.

---
+++ core/src/com/mygdx/game/sprites/Tube.java
@@ -0,0 +1,49 @@
+package com.mygdx.game.sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import java.security.PublicKey;
+import java.util.Random;
+
+public class Tube {
+    //добавим константу, это будет диапазон отклонений на котором буду создаватся трубы по высоте
+    private static final int FLUCTUATION = 130;
+    //харанит значение высоты просвета
+    public static final int TUBE_GAP = 100;
+    //хранит нижнию границу просвета
+    public static final int LOWEST_OPENNING = 120;
+
+    private Texture topTube, bottomTube;
+    private Vector2 posToptube, posBottube;
+    private Random rand;
+
+
+    public Texture getTopTube() {
+        return topTube;
+    }
+
+    public Texture getBottomTube() {
+        return bottomTube;
+    }
+
+    public Vector2 getPosToptube() {
+        return posToptube;
+    }
+
+    public Vector2 getPosBottube() {
+        return posBottube;
+    }
+
+    public Tube(float x ){
+        topTube = new Texture("wall.png");
+        bottomTube = new Texture("wall.png");
+        rand = new Random();
+
+        posToptube = new Vector2(x,rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENNING);
+        posBottube = new Vector2(x, posToptube.y - TUBE_GAP - bottomTube.getHeight());
+
+
+    }
+
+}
--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -1,35 +1,50 @@
 package com.mygdx.game.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.Nevermore;
 import com.mygdx.game.sprites.Bird;
+import com.mygdx.game.sprites.Tube;
 
 public class PlayState extends State {
     private Bird bird;
+    private Texture bg;
+    private Tube tube;
 
     //создаем экземпляр птицы в конструкторе
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 300);
+        bg = new Texture("bg.png");
         camera.setToOrtho(false, Nevermore.WIDTH / 2, Nevermore.HEIGHT / 2);
+        tube = new Tube(100);
     }
 
     @Override
     protected void handleInput() {
-
+        if (Gdx.input.justTouched()){
+            bird.jump();
+        }
     }
 
     @Override
     public void update(float dt) {
+        handleInput();
         bird.update(dt);
     }
 
     @Override
     public void render(SpriteBatch sb) {
+        sb.setProjectionMatrix(camera.combined);
         sb.begin();
+        sb.draw(bg, camera.position.x - (camera.viewportWidth / 2), 0);
         //передаем из класса bird текстуру + позицию
         sb.draw(bird.getBird(),bird.getPosition().x, bird.getPosition().y);
+        sb.draw(tube.getTopTube(), tube.getPosBottube().x, tube.getPosToptube().y);
+        sb.draw(tube.getBottomTube(), tube.getPosBottube().x, tube.getPosBottube().y);
+
+
         sb.end();
 
     }
